555155c19543d324a78d56ac95d6723cefacc41e
Manojbabu <83714923+manusoft@users.noreply.github.com>
2025-05-24 02:03:32 +0400

    Update project files and enhance user interaction
    Removed unused DLLs from `Models` and added them as content files in `OfflineAI.Demo.csproj`. Improved user input prompt and output formatting in `Program.cs` for better interaction.

--- sample/OfflineAI.Demo/Program.cs
+++ sample/OfflineAI.Demo/Program.cs
@@ -4,13 +4,12 @@ Console.WriteLine("Hi! ask me anything!");
 
 while (true)
 {
+    Console.Write("You: ");
     string question = Console.ReadLine();
 
-    var llama = new OfflineAIModel(
-        "Models/tinyllama-1.1b-chat-v1.0.Q8_0.gguf",
-        "Models/llama-cli.exe");
+    var llama = new OfflineAIModel("Models/tinyllama-1.1b-chat-v1.0.Q8_0.gguf", "Models/llama-cli.exe");
 
     var output = llama.GenerateText(question, 128);
 
-    Console.WriteLine(output);
+    Console.WriteLine($"AI: " + output);
 }
